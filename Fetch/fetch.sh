5b653483b66f7cc6c7934414a060a402b8addb57
Marcin Kielbasinski <marcin.kielbasinski@syncron.com>
2017-01-16 10:28:49 +0100

    Soemthing changed

--- Fetch/fetch.sh
+++ Fetch/fetch.sh
@@ -1,2 +1,15 @@
 #!/bin/bash
 echo "Hello World"
+curl -vs "http://mojeinwestycje.interia.pl/gie/notgpw/full/zlec_all?sp=55&zl=A" 2>&1 |
+grep -o "[0-9,]\+\s\.\+\s[0-9]\+\s\.\+\s[0-9:]\{8\}" |
+sed -n 's/\([0-9,]\+\)\s\.*\s\([0-9]\+\).*/\1|\2/p' |
+awk 'BEGIN {FS=OFS=SUBSEP="|"}{arr[$1]+=$2}END {for(i in arr) print i, arr[i]}' |
+sort -nr |
+awk 'BEGIN {FS=OFS=SUBSEP="|"}{p[NR]=$1; agg[NR]+=(agg[NR-1]+$2)}END {for(i in agg) print p[i], agg[i]}' |
+sort -n
+curl -vs "http://mojeinwestycje.interia.pl/gie/notgpw/full/zlec_all?sp=55&zl=V" 2>&1 |
+grep -o "[0-9,]\+\s\.\+\s[0-9]\+\s\.\+\s[0-9:]\{8\}" |
+sed -n 's/\([0-9,]\+\)\s\.*\s\([0-9]\+\).*/\1|\2/p' |
+awk 'BEGIN {FS=OFS=SUBSEP="|"}{arr[$1]+=$2}END {for(i in arr) print i, arr[i]}' |
+sort -n |
+awk 'BEGIN {FS=OFS=SUBSEP="|"}{p[NR]=$1; agg[NR]+=(agg[NR-1]+$2)}END {for(i in agg) print p[i], agg[i]}'
